e86335dfa914889d1ee0e93f7372a77a40170efc
Jacob <jackeaik@hotmail.com>
2018-11-26 22:24:16 +0100

    Fix: Styling for Resume page should now work properly

--- client/src/views/Resume/Resume.js
+++ client/src/views/Resume/Resume.js
@@ -4,29 +4,21 @@ import pdf from '../../assets/CV.pdf';
 
 import MainHero from '../.././components/MainHero';
 
-import { Button } from 'react-bulma-components/full';
-
 class Resume extends React.Component {
-  /*
-  <Section>
-    <Document
-      file={pdf}
-      onLoadSuccess={this.onDocumentLoadSuccess}
-    >
-      <Page className="pdf-page" pageNumber={1} />
-      <Page className="pdf-page" pageNumber={2} />
-    </Document>
-  </Section>
-  */
 
   render () {
     return (
       <div>
         <MainHero title="FIND OUT MORE" subtitle="Discover what I have done in my past"
-        button={  <a download="CV.pdf" href={pdf}>
-          <Button id="btn-resume" className="is-hidden-mobile">DOWNLOAD SWEDISH RESUME</Button>
-          <Button id="btn-resume-mobile" className="is-hidden-tablet">DOWNLOAD RESUME</Button>
-        </a>}
+        button={
+        <div>
+          <a download="CV.pdf" href={pdf} id="btn-resume" className="is-hidden-mobile button is-link">
+            DOWNLOAD SWEDISH RESUME
+          </a>
+          <a download="CV.pdf" href={pdf} id="btn-resume-mobile" className="is-hidden-tablet button is-link">
+            DOWNLOAD RESUME
+          </a>
+        </div>}
         fullheight="is-fullheight" background="has-bg-img-escalator"/>
 
       </div>
