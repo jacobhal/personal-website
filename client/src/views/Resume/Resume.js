27c993906e7b1d4f90bdab26737f1e4e7c700c19
Jacob <jackeaik@hotmail.com>
2018-11-27 17:45:41 +0100

    Fix: Resume page modified to work better in IE

--- client/src/views/Resume/Resume.js
+++ client/src/views/Resume/Resume.js
@@ -4,6 +4,8 @@ import pdf from '../../assets/CV.pdf';
 
 import MainHero from '../.././components/MainHero';
 
+import { Columns } from 'react-bulma-components/full';
+
 class Resume extends React.Component {
 
   render () {
@@ -11,14 +13,16 @@ class Resume extends React.Component {
       <div>
         <MainHero title="FIND OUT MORE" subtitle="Discover what I have done in my past"
         button={
-        <div>
-          <a download="CV.pdf" href={pdf} id="btn-resume" className="is-hidden-mobile button is-link">
-            DOWNLOAD SWEDISH RESUME
-          </a>
-          <a download="CV.pdf" href={pdf} id="btn-resume-mobile" className="is-hidden-tablet button is-link">
-            DOWNLOAD RESUME
-          </a>
-        </div>}
+        <Columns>
+          <Columns.Column>
+            <a download="CV.pdf" href={pdf} id="btn-resume" className="is-hidden-mobile button is-link">
+              DOWNLOAD SWEDISH RESUME
+            </a>
+            <a download="CV.pdf" href={pdf} id="btn-resume-mobile" className="is-hidden-tablet button is-link">
+              DOWNLOAD RESUME
+            </a>
+          </Columns.Column>
+        </Columns>}
         fullheight="is-fullheight" background="has-bg-img-escalator"/>
 
       </div>
