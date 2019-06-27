802e9147a675b53cd9c20cd5182b4305795993ac
Jacob <jackeaik@hotmail.com>
2019-06-27 18:30:56 +0200

    Add: English resume added

--- client/src/views/Resume/Resume.js
+++ client/src/views/Resume/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import pdf from '../../assets/CV.pdf';
+import pdf from '../../assets/CV_swedish.pdf';
+import pdf_english from '../../assets/CV_english.pdf';
 //import { Document, Page } from 'react-pdf';
 
 import MainHero from '../.././components/MainHero';
@@ -15,12 +16,26 @@ class Resume extends React.Component {
         button={
         <Columns>
           <Columns.Column>
-            <a download="CV.pdf" href={pdf} id="btn-resume" className="is-hidden-mobile button is-link">
+            <div>
+            <a download="CV_swedish.pdf" href={pdf} id="btn-resume" className="is-hidden-mobile button is-link">
               DOWNLOAD SWEDISH RESUME
             </a>
-            <a download="CV.pdf" href={pdf} id="btn-resume-mobile" className="is-hidden-tablet button is-link">
-              DOWNLOAD RESUME
-            </a>
+            </div>
+            <div>
+              <a download="CV_english.pdf" href={pdf_english} id="btn-resume-english" className="is-hidden-mobile button is-link">
+                DOWNLOAD ENGLISH RESUME
+              </a>
+            </div>
+            <div>
+              <a download="CV_swedish.pdf" href={pdf} id="btn-resume-mobile" className="is-hidden-tablet button is-link">
+                SWEDISH RESUME
+              </a>
+            </div>
+            <div>
+              <a download="CV_english.pdf" href={pdf_english} id="btn-resume-mobile-english" className="is-hidden-tablet button is-link">
+                ENGLISH RESUME
+              </a>
+            </div>
           </Columns.Column>
         </Columns>}
         fullheight="is-fullheight" background="has-bg-img-escalator"/>
