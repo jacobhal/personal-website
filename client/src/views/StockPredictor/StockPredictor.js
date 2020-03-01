81276e726d7d775f20ad2748f6a1aabb0e0a136f
Jacob <jackeaik@hotmail.com>
2020-03-01 22:23:01 +0100

    Added React Hooks

--- client/src/views/StockPredictor/StockPredictor.js
+++ client/src/views/StockPredictor/StockPredictor.js
@@ -6,21 +6,19 @@ import API from '../../services/API';;
 
 
 const StockPredictor = ()  => {
-    //const [stocks, setStocks] = useState({}) // Initial state value empty object
+    const [stocks, setStocks] = useState({}) // Initial state value empty object
     
     async function fetchData() {
         const myApi = new API({ url:'https://restful-stock-api.herokuapp.com' })
         myApi.createEntity({ name: 'getmsg' })
         myApi.endpoints.getmsg.getAll({ params: {name: 'Jacob'} })
             .then(res => res.data)
-            .then(data => console.log(data)) //setStocks(data)
+            .then(data => setStocks(data)) //
     }
 
-    /*
     useEffect(() => {
         fetchData();
     })
-    */
    const res = fetchData()
     return (
         <div>
@@ -32,7 +30,7 @@ const StockPredictor = ()  => {
             <Section>
                 <Container>
                     <h1 className="title">My API Data</h1>
-                    {/* { JSON.stringify(stocks) } */}
+                    { JSON.stringify(stocks) }
                 </Container>
             </Section>
         </div>
