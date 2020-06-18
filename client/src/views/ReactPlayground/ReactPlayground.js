c2b915f72fb52d414664a82c30609a8e632843ec
Jacob <jacobhallman94@gmail.com>
2020-06-18 16:37:54 +0200

    Fixed react playground editor issues with prism css classes clashing with bulmas

--- client/src/views/ReactPlayground/ReactPlayground.js
+++ client/src/views/ReactPlayground/ReactPlayground.js
@@ -1,12 +1,11 @@
 import React, {useState, useEffect, useContext, useMemo, useRef, useReducer} from 'react';
-// import React, { useState, useEffect, useRef } from 'react';
 
 import { NavBar } from '../../components/NavBar';
 import dracula from 'prism-react-renderer/themes/dracula';
 import nightOwl from "prism-react-renderer/themes/nightOwl";
+import './../../styles/reactPlayground.css';
 
 import { Hero, Section, Container} from 'react-bulma-components/full';
-
 import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live'
 
 const scope = {useState, useEffect, useContext, useMemo, useRef, useReducer};
@@ -32,8 +31,12 @@ const ReactPlayground = () => {
             <Section>
                 <Container>
                     <h1 className="title">React playground</h1>  
-                    <LiveProvider scope={scope} theme={dracula} code={exampleCode}>
-                        <LiveEditor collapsableCode={true} />
+                    <LiveProvider scope={scope} theme={dracula} code={exampleCode} >
+                        <LiveEditor className="content"
+                         style={{
+                            fontFamily: '"Fira code", "Fira Mono", monospace',
+                            fontSize: 16,
+                          }}/>
                         <LiveError />
                         <LivePreview />
                     </LiveProvider>
