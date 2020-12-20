8694eb0821d8f263a04f559b86314b2b375a45b0
Jacob Hallman <jackeaik@hotmail.com>
2020-12-20 14:43:49 +0100

    Navbar + jumbotron fixed for Home page

--- client/src/components/MainHero.js
+++ client/src/components/MainHero.js
@@ -6,9 +6,13 @@ import { NavBar } from './NavBar';
 class MainHero extends React.Component {
   render() {
     return (
-        <Jumbotron fluid color="black" style={{padding: '20px 50px'}} className={this.props.background + ' ' + this.props.fullheight}>
+        <Jumbotron fluid color="black" style={{padding: '20px 50px', height: '500px'}} className={this.props.background + ' ' + this.props.fullheight}>
             <NavBar />
-            <Container className="has-text-centered">
+            <Container className="has-text-centered jumbotron-content">
+              <h1 className="has-text-centered outline" subtitle size={6} id="jumbotron-subtitle">
+                {this.props.subtitle}
+              </h1>
+              <h3 className="has-text-centered outline" id="jumbotron-title">{this.props.title}</h3>
               {this.props.button}
             </Container>
         </Jumbotron>
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -10,24 +10,26 @@ const NavBar = () => {
   const styling = {height: '28px', width: '28px'};
   return (
     <Navbar expand="lg">
-    <Navbar.Brand href="/"> 
-              <img
-                src={logo}
-                alt="Jacob Hallman"
-                style={styling}
-              />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" style={{color: 'white !important'}}>
-      <Nav className="ml-auto" pullRight>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/resume">Resume</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+      <Navbar.Brand href="/"> 
+                <img
+                  src={logo}
+                  alt="Jacob Hallman"
+                  style={styling}
+                />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"> 
+        <i className="fa fa-bars"></i> 
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto" pullRight>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 
 
