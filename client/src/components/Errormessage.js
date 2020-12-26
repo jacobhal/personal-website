1578fb2885f1d01a2ccca89afe3f1ebe29f20be9
Jacob Hallman <jackeaik@hotmail.com>
2020-12-26 13:47:54 +0100

    Removed unused prop

--- client/src/components/Errormessage.js
+++ client/src/components/Errormessage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Alert } from 'react-bootstrap'
 
-const Errormessage = ({ children, title, topMargin }) => {
+const Errormessage = ({ children, title }) => {
     return (
         <Alert variant="danger">
             <Alert.Heading>{title}</Alert.Heading>
